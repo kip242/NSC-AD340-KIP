f588a6c23932e397e05175506aad5d342aec45da
Kip Tatum <kiptatum@gmail.com>
2017-04-21 19:35:34 -0700

    created AD340 project to update assignments on a single project

---
+++ AD340Project/app/src/main/java/com/example/kip/ad340project/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.kip.ad340project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.kip.assignment2.MESSAGE";
+    private static final String TAG = DisplayMessageActivity.class.getSimpleName();
+    String[] days;
+    //Button [] bt = new Button[4];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//creates layout
+        Log.d(TAG, "onCreate()");
+
+        days = getResources().getStringArray(R.array.days_array);
+       /* bt[0] = (Button)findViewById(R.id.button);
+        bt[1] = (Button)findViewById(R.id.button);
+        bt[2] = (Button)findViewById(R.id.button);
+        bt[3] = (Button)findViewById(R.id.button);*/
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, days);
+        gridview.setAdapter(adapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    /**Function call when "SEND" is pressed*/
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
